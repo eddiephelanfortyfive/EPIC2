6b5696e4d5eb53983868130d9a0131fa771739f5
eddiephelanfortyfive <23397179@studentmail.ul.ie>
2023-12-08 11:55:20 +0000

    LinkedList Fixed

--- src/Testy.java
+++
@@ -1,9 +0,0 @@
-public class Testy {
-    public static <T> void main(String []args){
-        LinkedList<T> list = new LinkedList<T>();
-        list.insert((T) "mund");
-        list.insert((T) "edmund");
-        list.printAllData();
-
-    }
-}
