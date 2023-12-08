d1665044cbffde237b788e62971ce33ba023bb76
eddiephelanfortyfive <23397179@studentmail.ul.ie>
2023-12-08 13:25:05 +0000

    Taxi data base and taxi array

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,2 +1,38 @@
 public class Taxi {
+    private String registration;
+    private String name;
+    private int rating;
+    private String brand;
+    private String size;
+    private int seats;
+
+
+    public Taxi(String registration, String name, int rating, String brand, String size, int seats){
+        this.registration=registration;
+        this.name=name;
+        this.rating=rating;
+        this.brand=brand;
+        this.size=size;
+        this.seats=seats;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getSize() {
+        return size;
+    }
+    public int getSeats() {
+        return seats;
+    }
+
 }
