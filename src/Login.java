a4a736c3753562d62c0537251cdcd15dc1c63494
eddiephelanfortyfive <23397179@studentmail.ul.ie>
2023-12-10 23:26:26 +0000

    switch statements used in the login in choice cases

--- src/Login.java
+++ src/Login.java
@@ -6,6 +6,8 @@ import java.util.stream.IntStream;
 public class Login {
     private String username;
     private String password;
+    private static final String OPTION_ZERO ="0";
+    private static final String OPTION_ONE ="1";
 
     public void loginOrSignUp(){
         Scanner input = new Scanner(System.in);
@@ -14,12 +16,16 @@ public class Login {
         System.out.println("---\nWelcome to the TaxiApp.\n Sign up [0]\n or\n Log in[1]");
         for(int i =0; i<1; i++) {
             String logInOrSignUp = input.nextLine();
-            if (logInOrSignUp.equals("0")) {
-                signUpToApp(usernames,text);
-            } else if (logInOrSignUp.equals("1")) {
-                loginToApp(usernames, text);
-            } else {--i;
-                System.out.println("Invalid input please try again.");
+            switch (logInOrSignUp) {
+                case OPTION_ZERO:
+                    signUpToApp(usernames, text);
+                    break;
+                case OPTION_ONE:
+                    loginToApp(usernames, text);
+                    break;
+                default:
+                    System.out.println("Invalid input, please try again.");
+                    i--;
             }
         }
     }
@@ -48,17 +54,18 @@ public class Login {
                 System.out.println("The account " + username + " does not exist.\n Would you like to try again?[0]\n Or\n Sign up?[1]");
                 for (int j = 0; j < 1; j++) {
                     String choice = input.nextLine();
-                    if (choice.equals("0")) {
-                        i--;
-                        System.out.println("You chose to try again.");
-
-                    } else if (choice.equals("1")) {
-                        System.out.println("You chose Sign up.");
-                        signUpToApp(usernames, text);
-                    } else {
-                        System.out.println("Invalid input please try again.");
-                        j--;
-
+                    switch (choice) {
+                        case OPTION_ZERO:
+                            i--;
+                            System.out.println("You chose to try again.");
+                            break;
+                        case OPTION_ONE:
+                            System.out.println("You chose Sign up.");
+                            signUpToApp(usernames, text);
+                            break;
+                        default:
+                            System.out.println("Invalid input please try again.");
+                            j--;
                     }
                 }
 
@@ -72,24 +79,26 @@ public class Login {
         System.out.println("---\nSign up enter your chosen username: ");
         String username=input.nextLine();
         String usernameLowerCase = username.toLowerCase();
-        if(doesContain(usernameLowerCase, usernames)!=-1) {
+        if(usernameLowerCase.equals(" ")) {
+            i--;
+        }else if(doesContain(usernameLowerCase, usernames)!=-1) {
             System.out.println("The account "+username+" already exists.\n Would you like to log in to this account?[0]\n Or\n Input a different username?[1]\n");
             for(int j=0; j<1; j++) {
                 String choice = input.nextLine();
-                if (choice.equals("0")) {
-                    System.out.println("You chose to login to the account "+username);
-                    loginToApp(usernames, text);
-                } else if (choice.equals("1")) {
-                    i--;
-                    System.out.println("You chose to input a different username.");
-                } else {
-                    System.out.println("Invalid input please try again.");
-                    j--;
-
+                switch (choice) {
+                    case OPTION_ZERO:
+                        System.out.println("You chose to login to the account "+username);
+                        loginToApp(usernames, text);
+                        break;
+                    case OPTION_ONE:
+                        i--;
+                        System.out.println("You chose to input a different username.");
+                        break;
+                    default:
+                        System.out.println("Invalid input please try again.");
+                        j--;
                 }
             }
-
-
         }else{
             System.out.println("---\nWelcome "+username+" please enter your chosen password:");
             String password= input.nextLine();
